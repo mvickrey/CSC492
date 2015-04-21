2a4faf5bc5608a05cd402abeafb37dbb6942daac
Kurtis Van Gent <kurtis.vangent@coyotes.usd.edu>
2015-04-20 19:56:05 -0500

    Revert "Revert "Added PowerUps""
    This reverts commit 8f373258cbf5973ebe0f7fe7589ca8b97ea008d5.

--- Game1Prototype/Assets/__Scripts/CameraFollow.cs
+++ Game1Prototype/Assets/__Scripts/CameraFollow.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CameraFollow : PT_MonoBehaviour {
 	public static CameraFollow S;
+	public GameObject prefabPowerUp;
 
 	public Transform	targetTransform;
 	public float 		camEasing = 0.1f;
@@ -15,6 +16,18 @@ public class CameraFollow : PT_MonoBehaviour {
 	void FixedUpdate(){
 		Vector3 pos1 = targetTransform.position + followOffset;
 		pos = Vector3.Lerp (pos, pos1, camEasing);
+	}
+
+	public void SpawnPowerUp(Enemy e){
+		GameObject go = Instantiate(prefabPowerUp) as GameObject;
+		PowerUp pu = go.GetComponent<PowerUp>();
+		int rnd = Random.Range(0,4);
+		pu.SetType(rnd);
+		Vector3 pos = e.transform.position;
+		pos.z -= .5f;
+		pu.transform.position = pos;
+		Debug.Log (pu.transform.position);
+		Debug.Log("Powerup created!");
 	}
 
 }
